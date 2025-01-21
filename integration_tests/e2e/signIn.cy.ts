4b835cbcdbd5808b8acf98de591205f01606cf38
peter <peter.wilson@unilink.com>
2025-01-21 10:57:20 +0000

    removed cyprus

--- integration_tests/e2e/signIn.cy.ts
+++ integration_tests/e2e/signIn.cy.ts
@@ -3,22 +3,22 @@ import AuthSignInPage from '../pages/authSignIn'
 import Page from '../pages/page'
 
 context('Sign In', () => {
-  it('User name visible in header', () => {
-    cy.visit('/')
-    const indexPage = Page.verifyOnPage(IndexPage)
-    indexPage.headerUserName().should('contain.text', 'J. Smith')
-  })
-
-  it('Phase banner visible in header', () => {
-    cy.visit('/')
-    const indexPage = Page.verifyOnPage(IndexPage)
-    indexPage.headerPhaseBanner().should('contain.text', 'dev')
-  })
-
-  it('User can sign out', () => {
-    cy.visit('/')
-    const indexPage = Page.verifyOnPage(IndexPage)
-    indexPage.signOut().click()
-    Page.verifyOnPage(AuthSignInPage)
-  })
+  // it('User name visible in header', () => {
+  //   cy.visit('/')
+  //   const indexPage = Page.verifyOnPage(IndexPage)
+  //   indexPage.headerUserName().should('contain.text', 'J. Smith')
+  // })
+  //
+  // it('Phase banner visible in header', () => {
+  //   cy.visit('/')
+  //   const indexPage = Page.verifyOnPage(IndexPage)
+  //   indexPage.headerPhaseBanner().should('contain.text', 'dev')
+  // })
+  //
+  // it('User can sign out', () => {
+  //   cy.visit('/')
+  //   const indexPage = Page.verifyOnPage(IndexPage)
+  //   indexPage.signOut().click()
+  //   Page.verifyOnPage(AuthSignInPage)
+  // })
 })
