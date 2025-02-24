4a7be6e87441a034b6ad226e7dcbb245ecbf94ec
rmccormack <rmccormack@unilink.com>
2025-02-24 15:24:26 +0000

    BR-21 : Report-Completed page

--- server/routes/reportCompleted.ts
+++ server/routes/reportCompleted.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express'
 import AuditService, { Page } from '../services/auditService'
+import BreachNoticeApiClient from '../data/breachNoticeApiClient'
 import { HmppsAuthClient } from '../data'
 
 export default function reportCompletedRoutes(
@@ -9,8 +10,14 @@ export default function reportCompletedRoutes(
 ): Router {
   router.get('/report-completed/:id', async (req, res, next) => {
     await auditService.logPageView(Page.REPORT_COMPLETED, { who: res.locals.user.username, correlationId: req.id })
-    await hmppsAuthClient.getSystemClientToken(res.locals.user.username)
-    res.render('pages/report-completed')
+    const breachNoticeApiClient = new BreachNoticeApiClient(
+      await hmppsAuthClient.getSystemClientToken(res.locals.user.username),
+    )
+    const { id } = req.params
+    const breachNotice = await breachNoticeApiClient.getBreachNoticeById(id as string)
+    res.render('pages/report-completed', {
+      breachNotice,
+    })
   })
   return router
 }
