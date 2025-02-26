05bf64c285446c6a5a1e0974707d49db4c034fa3
karlfosterBCL <karl.foster@bconline.co.uk>
2025-02-26 10:35:55 +0000

    integration tests

--- integration_tests/e2e/limitedAccess.cy.ts
+++ integration_tests/e2e/limitedAccess.cy.ts
@@ -1,21 +1,41 @@
-context('Navigation Menu data checks', () => {
-  it('New report', () => {
+context('Limited Access checks', () => {
+  it('Basic Details LAO Excluded', () => {
     cy.visit('/basic-details/00000000-0000-0000-0000-100000000006')
     cy.get('#lao-panel').should('exist')
   })
 
-  it('New report', () => {
+  it('Basic Details LAO Restricted', () => {
     cy.visit('/basic-details/00000000-0000-0000-0000-100000000005')
     cy.get('#lao-panel').should('exist')
   })
 
-  it('New report', () => {
+  it('Basic Details LAO Both', () => {
     cy.visit('/basic-details/00000000-0000-0000-0000-100000000007')
     cy.get('#lao-panel').should('exist')
   })
 
-  it('New report', () => {
+  it('Basic Details LAO None', () => {
     cy.visit('/basic-details/00000000-0000-0000-0000-100000000008')
+    cy.get('#lao-panel').should('not.exist')
+  })
+
+  it('Check your report LAO Excluded', () => {
+    cy.visit('/check-your-report/00000000-0000-0000-0000-100000000006')
+    cy.get('#lao-panel').should('exist')
+  })
+
+  it('Check your report LAO Restricted', () => {
+    cy.visit('/check-your-report/00000000-0000-0000-0000-100000000005')
+    cy.get('#lao-panel').should('exist')
+  })
+
+  it('Check your report LAO Both', () => {
+    cy.visit('/check-your-report/00000000-0000-0000-0000-100000000007')
+    cy.get('#lao-panel').should('exist')
+  })
+
+  it('Check your report LAO None', () => {
+    cy.visit('/check-your-report/00000000-0000-0000-0000-100000000008')
     cy.get('#lao-panel').should('not.exist')
   })
 })
