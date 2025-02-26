8613ff457663447771730582dc7eb2808e868da6
Rob McCormack <56261775+robertmccormackbconline@users.noreply.github.com>
2025-02-26 11:29:06 +0000

    BR-5 (#20)
    * BR-5 : Report Deleted Page
    
    * BR-45 : Tests for Report Deleted Page
    
    * Update server/views/pages/report-deleted.njk
    
    Co-authored-by: karlfosterBCL <39554570+karlfosterBCL@users.noreply.github.com>
    
    * BR-5: Updated Test message
    
    ---------
    
    Co-authored-by: karlfosterBCL <39554570+karlfosterBCL@users.noreply.github.com>

---
+++ integration_tests/e2e/reportDeleted.cy.ts
@@ -0,0 +1,17 @@
+context('Report Deleted page', () => {
+  it('displays correct panel message', () => {
+    cy.visit('/report-deleted/00000000-0000-0000-0000-000000000001')
+    cy.get('.govuk-panel--confirmation').should('exist')
+    cy.get('.govuk-panel--confirmation').should('exist').should('contain.text', 'Report Deleted')
+    cy.get('.govuk-panel--confirmation')
+      .should('exist')
+      .should('contain.text', 'This Breach Report has been permanently deleted and cannot be recovered')
+  })
+
+  it('delete link closes window', () => {
+    cy.visit('/report-deleted/00000000-0000-0000-0000-000000000001')
+    cy.get('#close-window').should('exist').should('contain.text', 'Click here to close this window')
+    cy.get('#close-window .govuk-link').click()
+    cy.url().should('include', '/close')
+  })
+})
--- server/routes/index.ts
+++ server/routes/index.ts
@@ -31,7 +31,7 @@ export default function routes({ auditService, hmppsAuthClient, snsService, comm
   warningDetailsRoutes(router, auditService)
   checkYourReportRoutes(router, auditService, hmppsAuthClient, snsService, commonUtils)
   pdfMaintenanceRoutes(router, auditService, hmppsAuthClient)
-  reportDeletedRoutes(router, auditService)
+  reportDeletedRoutes(router, auditService, hmppsAuthClient)
   reportCompletedRoutes(router, auditService, hmppsAuthClient)
   nextAppointmentRoutes(router, auditService, hmppsAuthClient, commonUtils)
   return router
