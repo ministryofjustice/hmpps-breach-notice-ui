3e5906ca00caa4f99d31bd0f6f2b75554df83951
karlfosterBCL <39554570+karlfosterBCL@users.noreply.github.com>
2025-03-14 13:52:12 +0000

    Feature/br 59 domain events (#50)
    * initial set
    
    * changed wording of message
    
    * BR-59, BR-65 - Merge and Unmerge events
    
    * moved publish to kotlin

--- server/routes/checkYourReport.ts
+++ server/routes/checkYourReport.ts
@@ -2,10 +2,7 @@ import { type RequestHandler, Response, Router } from 'express'
 import AuditService, { Page } from '../services/auditService'
 import BreachNoticeApiClient, { BreachNotice } from '../data/breachNoticeApiClient'
 import { HmppsAuthClient } from '../data'
-import SnsService from '../services/snsService'
 import CommonUtils from '../services/commonUtils'
-import { HmppsDomainEvent } from '../data/hmppsSnsClient'
-import config from '../config'
 import { toUserDate, toUserDateFromDateTime, toUserTimeFromDateTime } from '../utils/dateUtils'
 import asyncMiddleware from '../middleware/asyncMiddleware'
 import { ErrorMessages } from '../data/uiModels'
@@ -14,7 +11,6 @@ export default function checkYourReportRoutes(
   router: Router,
   auditService: AuditService,
   hmppsAuthClient: HmppsAuthClient,
-  snsService: SnsService,
   commonUtils: CommonUtils,
 ): Router {
   const get = (path: string | string[], handler: RequestHandler) => router.get(path, asyncMiddleware(handler))
@@ -86,28 +82,6 @@ export default function checkYourReportRoutes(
 
     breachNotice.completedDate = new Date()
     await breachNoticeApiClient.updateBreachNotice(id, breachNotice)
-
-    const baseUrl: string = config.apis.breachNotice.url
-    const event: HmppsDomainEvent = {
-      eventType: 'probation-case.breach-notice.created',
-      version: 1,
-      description: 'A breach notice has been completed for a person on probation',
-      detailUrl: `${baseUrl}/pdf/${breachNotice.id}`,
-      occurredAt: new Date().toISOString(),
-      additionalInformation: {
-        breachNoticeId: `${breachNotice.id}`,
-      },
-      personReference: {
-        identifiers: [
-          {
-            type: 'CRN',
-            value: `${breachNotice.crn}`,
-          },
-        ],
-      },
-    }
-
-    snsService.sendMessage(event)
 
     res.redirect(`/report-completed/${req.params.id}`)
   })
