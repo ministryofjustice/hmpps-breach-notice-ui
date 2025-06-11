b0d33946bc1e925da220ed2e40c804725d11e6e3
Rob McCormack <56261775+robertmccormackbconline@users.noreply.github.com>
2025-06-11 16:00:36 +0100

    BR-199 (#138)

--- server/routes/reportDeleted.ts
+++ server/routes/reportDeleted.ts
@@ -1,20 +1,9 @@
 import { Router } from 'express'
 import AuditService, { Page } from '../services/auditService'
-import BreachNoticeApiClient from '../data/breachNoticeApiClient'
-import { HmppsAuthClient } from '../data'
 
-export default function reportDeletedRoutes(
-  router: Router,
-  auditService: AuditService,
-  hmppsAuthClient: HmppsAuthClient,
-): Router {
+export default function reportDeletedRoutes(router: Router, auditService: AuditService): Router {
   router.get('/report-deleted/:id', async (req, res, next) => {
     await auditService.logPageView(Page.REPORT_DELETED, { who: res.locals.user.username, correlationId: req.id })
-    const breachNoticeApiClient = new BreachNoticeApiClient(
-      await hmppsAuthClient.getSystemClientToken(res.locals.user.username),
-    )
-    const { id } = req.params
-    await breachNoticeApiClient.deleteBreachNotice(id as string)
 
     res.render('pages/report-deleted')
   })
