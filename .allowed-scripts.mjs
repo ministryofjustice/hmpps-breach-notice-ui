cb32c1277cab2cbe3f9627b726b3a9dc23262a9b
peter wilson <pwilson@unilink.com>
2026-07-17 15:21:02 +0100

    moj frontend to version 9, application insights to 3.15.1, govuk frontend to 6.4.0

--- .allowed-scripts.mjs
+++ .allowed-scripts.mjs
@@ -8,5 +8,6 @@ export default configureAllowedScripts({
     'node_modules/esbuild@0.25.10': 'ALLOW',
     'node_modules/fsevents@2.3.3': 'ALLOW',
     'node_modules/unrs-resolver@1.9.2': 'ALLOW',
+    'node_modules/protobufjs@7.6.5': 'ALLOW',
   },
 })
